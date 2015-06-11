17cc4ac451197104244cad9f5070ddad61d56754
James Sutton <james.sutton@uk.ibm.com>
2015-06-11 16:02:50 +0100

    Updating copyright headers in utility.js and index.html
    Signed-off-by: James Sutton <james.sutton@uk.ibm.com>

--- utility/utility.js
+++ utility/utility.js
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2013, 2014 IBM Corp.
+ * Copyright (c) 2015 IBM Corp.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -10,6 +10,8 @@
  * and the Eclipse Distribution License is available at
  *   http://www.eclipse.org/org/documents/edl-v10.php.
  *
+ * Contributors:
+ *    James Sutton - Initial Contribution
  *******************************************************************************/
 
 /*
