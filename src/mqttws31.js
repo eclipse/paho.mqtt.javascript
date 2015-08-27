f0cd0c653eb04892d4fea379f18e3db5fffc73cc
James Sutton <james.sutton@uk.ibm.com>
2015-08-27 13:55:29 +0100

    Bug: 476039 - Connect Options Validation does not validate all possible fields
    Added mqttVersionExlpicit and uris as valid fields.
    
    Signed-off-by: James Sutton <james.sutton@uk.ibm.com>

--- src/mqttws31.js
+++ src/mqttws31.js
@@ -1716,7 +1716,9 @@ Paho.MQTT = (function (global) {
 									   onFailure:"function",
 									   hosts:"object",
 									   ports:"object",
-									   mqttVersion:"number"});
+									   mqttVersion:"number",
+									   mqttVersionExplicit:"boolean",
+									   uris: "object"});
 			
 			// If no keep alive interval is set, assume 60 seconds.
 			if (connectOptions.keepAliveInterval === undefined)
